b115ed0b6c3f7b6d9c42ab88af06c1ece78c4b04
lytvynov <sLUqSzWNtN1N4yUVrykz>
2022-01-31 14:20:43 +0200

    Initial commit

---
+++ api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
@@ -0,0 +1,23 @@
+package se.magnus.api.core.recommendation;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface RecommendationService {
+
+    @GetMapping(
+            value = "/recommendation",
+            produces = "application/json")
+    Flux<Recommendation> getRecommendations(
+            @RequestHeader HttpHeaders headers,
+            @RequestParam(value = "productId") int productId);
+
+    Mono<Recommendation> createRecommendation(@RequestBody Recommendation body);
+
+    Mono<Void> deleteRecommendations(@RequestParam(value = "productId") int productId);
+}
